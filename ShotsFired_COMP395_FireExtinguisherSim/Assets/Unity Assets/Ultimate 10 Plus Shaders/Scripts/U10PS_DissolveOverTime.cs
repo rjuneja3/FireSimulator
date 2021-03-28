a694580952f3d74627214848bbed85773ebb5f3f
GearShark123 <lylespurrell@hotmail.com>
2021-03-28 06:00:14 -0400

    Modified shader materials to make fire effects
    - Dissolve
    - Plasma (1)

--- ShotsFired_COMP395_FireExtinguisherSim/Assets/Unity Assets/Ultimate 10 Plus Shaders/Scripts/U10PS_DissolveOverTime.cs
+++ ShotsFired_COMP395_FireExtinguisherSim/Assets/Unity Assets/Ultimate 10 Plus Shaders/Scripts/U10PS_DissolveOverTime.cs
@@ -9,17 +9,28 @@ public class U10PS_DissolveOverTime : MonoBehaviour
 
     public float speed = .5f;
 
-    private void Start(){
+    private void Start()
+    {
         meshRenderer = this.GetComponent<MeshRenderer>();
     }
 
-    private float t = 0.0f;
-    private void Update(){
+    private float t = 20.0f;
+    private void Update()
+    {
         Material[] mats = meshRenderer.materials;
+
+
+        if (Mathf.Sin(t * speed) < 0)
+        {
+            t = 20.0f;
+        }
+
+        //Debug.Log(Mathf.Sin(t * speed));
+        //Debug.Log(t);
 
         mats[0].SetFloat("_Cutoff", Mathf.Sin(t * speed));
         t += Time.deltaTime;
-        
+
         // Unity does not allow meshRenderer.materials[0]...
         meshRenderer.materials = mats;
     }
