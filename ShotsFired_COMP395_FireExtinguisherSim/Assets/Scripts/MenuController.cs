66494f179cd1b4bfb3fe84e2da235d3c3e5c07c3
Timothy Garcia <timothyiangarcia@gmail.com>
2021-03-31 15:49:09 -0400

    added menu and options scene (WIP)

---
+++ ShotsFired_COMP395_FireExtinguisherSim/Assets/Scripts/MenuController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    /// <summary>
+    /// THIS SCRIPT WILL BE HANDLING ALL FUNCTIONS USED BY THE MENU, END GAME SCENE, 
+    /// OPTION AND PAUSE MENU.
+    /// </summary>
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    void Update()
+    {
+    }
+
+    //Menu and end game scene functions.
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ToSettings()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void DifficultyScene()
+    {
+        SceneManager.LoadScene("DifficultyScene");
+    }
+    //Option function(s).
+    public AudioMixer audioMixer;
+    public void SetVolume(float vol)
+    {
+        Debug.Log(vol);
+        audioMixer.SetFloat("Volume", vol);
+    }
+}
