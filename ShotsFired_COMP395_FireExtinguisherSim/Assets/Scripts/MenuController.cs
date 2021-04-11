5c46a48a91d2f6a7dce159cda172c65c027e48ce
Timothy Garcia <timothyiangarcia@gmail.com>
2021-04-10 21:58:08 -0400

    music to menu scene added

--- ShotsFired_COMP395_FireExtinguisherSim/Assets/Scripts/MenuController.cs
+++ ShotsFired_COMP395_FireExtinguisherSim/Assets/Scripts/MenuController.cs
@@ -14,6 +14,7 @@ public class MenuController : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicScript>().PlayMusic();
     }
     void Update()
     {
---
+++ ShotsFired_COMP395_FireExtinguisherSim/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
