9fa3a6659f224b71d7fae8571303035fa237d89e
ttran229 <73140526+ttran229@users.noreply.github.com>
2021-04-13 18:23:18 -0400

    Fixed Shooting FE script, Added Fuel capacity for FE

--- ShotsFired_COMP395_FireExtinguisherSim/Assets/Scripts/GunScript.cs
+++ ShotsFired_COMP395_FireExtinguisherSim/Assets/Scripts/GunScript.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+    public int fuel;
     public GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Renderer feBullet = bullet.GetComponent<Renderer>();
+        feBullet.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        Renderer feBullet = bullet.GetComponent<Renderer>();
+        if (Input.GetKey(KeyCode.Mouse0) && fuel > 0)
         {
-            Renderer feBullet = bullet.GetComponent<Renderer>();
-            if (Input.GetMouseButton(0))
-            {
-                if (feBullet.enabled)
-                {
-                    feBullet.enabled = false;
-                }
-                else
-                    feBullet.enabled = true;
-
-            }
+            // if (Input.GetMouseButton(0))
+            // {
+            //if (feBullet.enabled)
+            //{
+            //    feBullet.enabled = false;
+            //}
+            //else
+            feBullet.enabled = true;
+            fuel--;
         }
+        else 
+            feBullet.enabled = false;
+        
     }
 }
